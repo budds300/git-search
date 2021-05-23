327015601414002fb0a6d060cb0a644debffb980
budds300 <budds300@gmail.com>
2021-05-23 13:05:33 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ReposComponent } from './repos/repos.component';
+import { UsersProfileComponent } from './users-profile/users-profile.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  
+  {path: '', component:UsersProfileComponent},
+  {path: 'profile', component:UsersProfileComponent},
+  {path:'repo',component:ReposComponent},
+  {path:'**', component:PageNotFoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
